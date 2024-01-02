515db96f74a21cf3a32483e37775af311c59b023
daqu <georgedaqu@gmail.com>
2024-01-02 16:28:52 +0400

    hero process

--- wp-content/themes/rgnutritionist/front-page.php
+++ wp-content/themes/rgnutritionist/front-page.php
@@ -1,5 +1,14 @@
 <?php get_header(); ?>
 
-
+<section class="hero">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/curve-gray-top.svg" alt="" class="curves top">
+  <div class="hero_content">
+    <figure>
+      <img src="<?php echo get_template_directory_uri(); ?>/images/gamarjoba.svg" alt="გამარჯობა">
+    </figure>
+    <div class="text">ეს არის საცდელი ტექსტი, რომელიც მოცემულია, როგორც მაგალითი</div>
+  </div>
+  <img src="<?php echo get_template_directory_uri(); ?>/images/curve-gray-bot.svg" alt="" class="curves bot">
+</section>
 
 <?php get_footer(); ?>
