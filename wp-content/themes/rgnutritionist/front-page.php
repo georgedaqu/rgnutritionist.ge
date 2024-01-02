1f13b8105925be864e3b7978b031ea18c7379adc
daqu <georgedaqu@gmail.com>
2024-01-02 16:48:05 +0400

    vegan one

--- wp-content/themes/rgnutritionist/front-page.php
+++ wp-content/themes/rgnutritionist/front-page.php
@@ -2,13 +2,21 @@
 
 <section class="hero">
   <img src="<?php echo get_template_directory_uri(); ?>/images/curve-gray-top.svg" alt="" class="curves top">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/curve-gray-bot.svg" alt="" class="curves bot">
   <div class="hero_content">
     <figure>
       <img src="<?php echo get_template_directory_uri(); ?>/images/gamarjoba.svg" alt="გამარჯობა">
     </figure>
     <div class="text">ეს არის საცდელი ტექსტი, რომელიც მოცემულია, როგორც მაგალითი</div>
   </div>
-  <img src="<?php echo get_template_directory_uri(); ?>/images/curve-gray-bot.svg" alt="" class="curves bot">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/vegan-1.png" alt="" class="vegan one">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/vegan-2.png" alt="" class="vegan two">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/vegan-3.png" alt="" class="vegan three">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/vegan-4.png" alt="" class="vegan four">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/vegan-5.png" alt="" class="vegan five">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/vegan-6.png" alt="" class="vegan six">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/vegan-7.png" alt="" class="vegan seven">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/vegan-8.png" alt="" class="vegan eight">
 </section>
 
 <?php get_footer(); ?>
