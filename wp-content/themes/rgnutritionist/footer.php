92275209d52b4f6e53d2a2e83e02fc2fdb4b48a4
daqu <georgedaqu@gmail.com>
2024-01-03 12:01:20 +0400

    svg

--- wp-content/themes/rgnutritionist/footer.php
+++ wp-content/themes/rgnutritionist/footer.php
@@ -1,13 +1,27 @@
 <script>
-	var main_slider = new Swiper(".main_slider", {
-		speed: 1000,
-		loop: true,
-		slidesPerView: 1,
-		navigation: {
-			nextEl: ".main_slider_next",
-			prevEl: ".main_slider_prev",
-		}
-	});
+var main_slider = new Swiper(".main_slider", {
+  speed: 1000,
+  loop: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".main_slider_next",
+    prevEl: ".main_slider_prev",
+  }
+});
+$('img.svg-icon').each(function() {
+  var $img = $(this);
+  var imgURL = $img.attr('src');
+  $.get(
+    imgURL,
+    function(data) {
+      var $svg = $(data).find('svg');
+      $svg = $svg.attr('class', $img.attr('class'));
+      $svg = $svg.removeAttr('xmlns:a');
+      $img.replaceWith($svg);
+    },
+    'xml'
+  );
+});
 </script>
 
 <?php wp_footer(); ?>
--- wp-content/themes/rgnutritionist/front-page.php
+++ wp-content/themes/rgnutritionist/front-page.php
@@ -22,34 +22,34 @@
 </section>
 
 <script>
-(function($) {
-  $.fn.extend({
-    jParallax: function(opt) {
-      var defaults = {
-          moveFactor: 5,
-          targetContainer: 'body'
-        },
-        o = $.extend(defaults, opt);
-      return this.each(function() {
-        var background = $(this);
-        $(o.targetContainer).on('mousemove', function(e) {
-          mouseX = e.pageX;
-          mouseY = e.pageY;
-          windowWidth = $(window).width();
-          windowHeight = $(window).height();
-          percentX = (0 - ((mouseX / windowWidth) * o.moveFactor) - (o.moveFactor / 2) + o.moveFactor) / 2;
-          percentY = (0 - ((mouseY / windowHeight) * o.moveFactor) - (o.moveFactor / 2) + o.moveFactor) / 2;
-          background[0].style.transform = "translate(" + percentX + "%," + percentY + "%)";
+  (function($) {
+    $.fn.extend({
+      jParallax: function(opt) {
+        var defaults = {
+            moveFactor: 5,
+            targetContainer: 'body'
+          },
+          o = $.extend(defaults, opt);
+        return this.each(function() {
+          var background = $(this);
+          $(o.targetContainer).on('mousemove', function(e) {
+            mouseX = e.pageX;
+            mouseY = e.pageY;
+            windowWidth = $(window).width();
+            windowHeight = $(window).height();
+            percentX = (0 - ((mouseX / windowWidth) * o.moveFactor) - (o.moveFactor / 2) + o.moveFactor) / 2;
+            percentY = (0 - ((mouseY / windowHeight) * o.moveFactor) - (o.moveFactor / 2) + o.moveFactor) / 2;
+            background[0].style.transform = "translate(" + percentX + "%," + percentY + "%)";
+          });
         });
-      });
-    }
-  });
-}(jQuery));
+      }
+    });
+  }(jQuery));
 
-$('.one, .two, .three, .four, .five, .six, .seven, .eight, .hero_title, .hero_text').jParallax({
-  moveFactor: 50,
-  targetContainer: '.hero'
-});
+  $('.one, .two, .three, .four, .five, .six, .seven, .eight, .hero_title, .hero_text').jParallax({
+    moveFactor: 50,
+    targetContainer: '.hero'
+  });
 </script>
 
 <section class="front_about trans-all-2">
@@ -63,11 +63,7 @@ $('.one, .two, .three, .four, .five, .six, .seven, .eight, .hero_title, .hero_te
         ?>
         <a href="<?php echo $about_link['url']; ?>" title="<?php echo $about_link['title']; ?>" class="btn btn-light btn-icon-right">
           <span><?php echo $about_link['title']; ?></span>
-          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10.0001 18.3334C14.6024 18.3334 18.3334 14.6024 18.3334 10C18.3334 5.39765 14.6024 1.66669 10.0001 1.66669C5.39771 1.66669 1.66675 5.39765 1.66675 10C1.66675 14.6024 5.39771 18.3334 10.0001 18.3334Z" stroke="#667085" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M7.08325 10H12.0833" stroke="#667085" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M10.4167 12.5L12.9167 10L10.4167 7.5" stroke="#667085" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/arrow.svg" alt="" class="svg-icon">
         </a>
       </div>
     </div>
