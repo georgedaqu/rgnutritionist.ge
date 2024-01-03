2c8303635b1521d2c8b7e85bf2a563d52bf1b4bd
daqu <georgedaqu@gmail.com>
2024-01-03 11:18:42 +0400

    about start

--- wp-content/themes/rgnutritionist/front-page.php
+++ wp-content/themes/rgnutritionist/front-page.php
@@ -52,4 +52,29 @@
   });
 </script>
 
+<section class="about_wrap trans-all-2">
+  <div class="container">
+    <div class="about_content">
+      <h2><?php echo get_field("about_title"); ?></h2>
+      <div class="about_text"><?php echo get_field("about_text"); ?></div>
+      <div class="more">
+        <?php
+        $about_link = get_field("about_link");
+        ?>
+        <a href="<?php echo $about_link['url']; ?>" title="<?php echo $about_link['title']; ?>" class="btn btn-light btn-icon-right">
+          <span><?php echo $about_link['title']; ?></span>
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.0001 18.3334C14.6024 18.3334 18.3334 14.6024 18.3334 10C18.3334 5.39765 14.6024 1.66669 10.0001 1.66669C5.39771 1.66669 1.66675 5.39765 1.66675 10C1.66675 14.6024 5.39771 18.3334 10.0001 18.3334Z" stroke="#667085" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M7.08325 10H12.0833" stroke="#667085" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M10.4167 12.5L12.9167 10L10.4167 7.5" stroke="#667085" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <figure>
+      <img src="<?php echo get_field("about_image"); ?>" alt="">
+    </figure>
+  </div>
+</section>
+
 <?php get_footer(); ?>
