d7caa147b2adad6227546d26588825da40294676
daqu <georgedaqu@gmail.com>
2024-01-05 16:56:06 +0400

    portfolio markup finish

--- wp-content/themes/rgnutritionist/templates/about-us.php
+++ wp-content/themes/rgnutritionist/templates/about-us.php
@@ -56,6 +56,22 @@ get_header();
             <figcaption>ვიდეოს სახელის ტექსტი</figcaption>
           </figure>
         </article>
+        <article>
+          <figure>
+            <a href="https://www.youtube.com/watch?v=zXCElopXzfs" class="magnific_video">
+              <img src="<?php echo get_template_directory_uri(); ?>/images/portfolio-2.jpg" alt="">
+            </a>
+            <figcaption>ვიდეოს სახელის ტექსტი</figcaption>
+          </figure>
+        </article>
+        <article>
+          <figure>
+            <a href="https://www.youtube.com/watch?v=8W0aGo_dMtM" class="magnific_video">
+              <img src="<?php echo get_template_directory_uri(); ?>/images/portfolio-3.jpg" alt="">
+            </a>
+            <figcaption>ვიდეოს სახელის ტექსტი</figcaption>
+          </figure>
+        </article>
       </div>
       <div class="see_all trans-all-2">
         <a href="javascript:void(0);" title="ნახე ყველა" class="btn btn-transparent btn-icon-right">
@@ -94,6 +110,14 @@ get_header();
             <figcaption>ვიდეოს სახელის ტექსტი</figcaption>
           </figure>
         </article>
+        <article>
+          <figure>
+            <a href="https://www.youtube.com/watch?v=8W0aGo_dMtM" class="magnific_video">
+              <img src="<?php echo get_template_directory_uri(); ?>/images/portfolio-3.jpg" alt="">
+            </a>
+            <figcaption>ვიდეოს სახელის ტექსტი</figcaption>
+          </figure>
+        </article>
       </div>
       <div class="see_all trans-all-2">
         <a href="javascript:void(0);" title="ნახე ყველა" class="btn btn-transparent btn-icon-right">
@@ -108,16 +132,35 @@ get_header();
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/scripts/magnific/magnific.css">
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/magnific/magnific.js"></script>
 <script>
-  if ($(".magnific_video").length) {
-    $(".magnific_video").magnificPopup({
-      disableOn: 700,
-      type: 'iframe',
-      mainClass: 'mfp-fade',
-      removalDelay: 160,
-      preloader: false,
-      fixedContentPos: false
-    });
+$(".portfolio .portfolio_item .container .portfolio_items").each(function() {
+  var $this = $(this);
+  var $portfolio_items = $this.find("article").length;
+  if ($portfolio_items > 3) {
+    $this.next(".see_all").css({
+      "display": "flex"
+    })
   }
+});
+$(".see_all").each(function() {
+  var $this = $(this);
+  $this.click(function() {
+    $this.prev(".portfolio_items").find("article").css({
+      "display": "block"
+    });
+    $this.hide();
+  });
+});
+
+if ($(".magnific_video").length) {
+  $(".magnific_video").magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+}
 </script>
 
 <?php get_footer(); ?>
