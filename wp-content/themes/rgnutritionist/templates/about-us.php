755fd293cd623103ae51b0b3c4751bcd7cb62170
daqu <georgedaqu@gmail.com>
2024-01-05 19:22:02 +0400

    testimonials markup finish

--- wp-content/themes/rgnutritionist/templates/about-us.php
+++ wp-content/themes/rgnutritionist/templates/about-us.php
@@ -31,50 +31,50 @@ get_header();
   $portfolio_row = get_field("portfolio_row");
   ?>
   <?php foreach ($portfolio_row as $portfolio_item) : ?>
-    <?php
+  <?php
     $portfolio_row_title = $portfolio_item["portfolio_row_title"];
     $portfolio_item = $portfolio_item["portfolio_item"];
     ?>
-    <div class="portfolio_item">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/worm-right.svg" alt="" class="worm">
-      <div class="container">
-        <h2><?php echo $portfolio_row_title; ?></h2>
-        <div class="portfolio_items">
-          <?php foreach ($portfolio_item as $item) : ?>
-            <?php
+  <div class="portfolio_item">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/worm-right.svg" alt="" class="worm">
+    <div class="container">
+      <h2><?php echo $portfolio_row_title; ?></h2>
+      <div class="portfolio_items">
+        <?php foreach ($portfolio_item as $item) : ?>
+        <?php
             $portfolio_video = $item["portfolio_video"];
             $portfolio_title = $item["portfolio_title"];
             ?>
-            <article>
-              <figure>
-                <a href="<?php echo $portfolio_video; ?>" class="magnific_video">
-                  <?php
+        <article>
+          <figure>
+            <a href="<?php echo $portfolio_video; ?>" class="magnific_video">
+              <?php
                   $url = $portfolio_video;
                   parse_str(parse_url($url, PHP_URL_QUERY), $query_params);
                   $video_id = $query_params['v'];
                   ?>
-                  <img src="https://i.ytimg.com/vi/<?php echo $video_id; ?>/hqdefault.jpg" alt="">
-                </a>
-                <figcaption><?php echo $portfolio_title; ?></figcaption>
-              </figure>
-            </article>
-          <?php endforeach; ?>
-        </div>
-        <div class="see_all trans-all-2">
-          <a href="javascript:void(0);" title="ნახე ყველა" class="btn btn-transparent btn-icon-right">
-            <span>ნახე ყველა</span>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-down.svg" alt="" class="svg-icon">
-          </a>
-        </div>
+              <img src="https://i.ytimg.com/vi/<?php echo $video_id; ?>/hqdefault.jpg" alt="">
+            </a>
+            <figcaption><?php echo $portfolio_title; ?></figcaption>
+          </figure>
+        </article>
+        <?php endforeach; ?>
+      </div>
+      <div class="see_all trans-all-2">
+        <a href="javascript:void(0);" title="ნახე ყველა" class="btn btn-transparent btn-icon-right">
+          <span>ნახე ყველა</span>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-down.svg" alt="" class="svg-icon">
+        </a>
       </div>
     </div>
+  </div>
   <?php endforeach; ?>
 </section>
 
 <section class="testimoinals">
   <div class="container">
     <div class="testimonial_body">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/testimonial.svg" alt="" class="border">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/testimonial.svg" alt="" class="border svg-icon">
       <img src="<?php echo get_template_directory_uri(); ?>/images/quot.svg" alt="" class="quot">
       <div class="testimonial_slider swiper">
         <div class="swiper-wrapper">
@@ -108,34 +108,34 @@ get_header();
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/scripts/magnific/magnific.css">
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/magnific/magnific.js"></script>
 <script>
-  $(".portfolio .portfolio_item .container .portfolio_items").each(function() {
-    var $this = $(this);
-    var $portfolio_items = $this.find("article").length;
-    if ($portfolio_items > 3) {
-      $this.next(".see_all").css({
-        "display": "flex"
-      })
-    }
-  });
-  $(".see_all").each(function() {
-    var $this = $(this);
-    $this.click(function() {
-      $this.prev(".portfolio_items").find("article").css({
-        "display": "block"
-      });
-      $this.hide();
-    });
-  });
-  if ($(".magnific_video").length) {
-    $(".magnific_video").magnificPopup({
-      disableOn: 700,
-      type: 'iframe',
-      mainClass: 'mfp-fade',
-      removalDelay: 160,
-      preloader: false,
-      fixedContentPos: false
-    });
+$(".portfolio .portfolio_item .container .portfolio_items").each(function() {
+  var $this = $(this);
+  var $portfolio_items = $this.find("article").length;
+  if ($portfolio_items > 3) {
+    $this.next(".see_all").css({
+      "display": "flex"
+    })
   }
+});
+$(".see_all").each(function() {
+  var $this = $(this);
+  $this.click(function() {
+    $this.prev(".portfolio_items").find("article").css({
+      "display": "block"
+    });
+    $this.hide();
+  });
+});
+if ($(".magnific_video").length) {
+  $(".magnific_video").magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+}
 </script>
 
 <?php get_footer(); ?>
