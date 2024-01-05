e7a9b0cabd0a2afbf7b729ff11c29523cb8f6699
daqu <georgedaqu@gmail.com>
2024-01-06 02:33:33 +0400

    about us finish

--- wp-content/themes/rgnutritionist/inc/translations.php
+++ wp-content/themes/rgnutritionist/inc/translations.php
@@ -5,4 +5,5 @@ add_action('init', function () {
 	pll_register_string('collaboration', 'Collaboration');
 	pll_register_string('blog', 'Blog');
 	pll_register_string('all_articles', 'All articles');
+	pll_register_string('see_all', 'See All');
 });
--- wp-content/themes/rgnutritionist/templates/about-us.php
+++ wp-content/themes/rgnutritionist/templates/about-us.php
@@ -31,77 +31,77 @@ get_header();
   $portfolio_row = get_field("portfolio_row");
   ?>
   <?php foreach ($portfolio_row as $portfolio_item) : ?>
-    <?php
+  <?php
     $portfolio_row_title = $portfolio_item["portfolio_row_title"];
     $portfolio_item = $portfolio_item["portfolio_item"];
     ?>
-    <div class="portfolio_item">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/worm-right.svg" alt="" class="worm">
-      <div class="container">
-        <h2><?php echo $portfolio_row_title; ?></h2>
-        <div class="portfolio_items">
-          <?php foreach ($portfolio_item as $item) : ?>
-            <?php
+  <div class="portfolio_item">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/worm-right.svg" alt="" class="worm">
+    <div class="container">
+      <h2><?php echo $portfolio_row_title; ?></h2>
+      <div class="portfolio_items">
+        <?php foreach ($portfolio_item as $item) : ?>
+        <?php
             $portfolio_video = $item["portfolio_video"];
             $portfolio_title = $item["portfolio_title"];
             ?>
-            <article>
-              <figure>
-                <a href="<?php echo $portfolio_video; ?>" class="magnific_video">
-                  <?php
+        <article>
+          <figure>
+            <a href="<?php echo $portfolio_video; ?>" class="magnific_video">
+              <?php
                   $url = $portfolio_video;
                   parse_str(parse_url($url, PHP_URL_QUERY), $query_params);
                   $video_id = $query_params['v'];
                   ?>
-                  <img src="https://i.ytimg.com/vi/<?php echo $video_id; ?>/hqdefault.jpg" alt="">
-                </a>
-                <figcaption><?php echo $portfolio_title; ?></figcaption>
-              </figure>
-            </article>
-          <?php endforeach; ?>
-        </div>
-        <div class="see_all trans-all-2">
-          <a href="javascript:void(0);" title="ნახე ყველა" class="btn btn-transparent btn-icon-right">
-            <span>ნახე ყველა</span>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-down.svg" alt="" class="svg-icon">
-          </a>
-        </div>
+              <img src="https://i.ytimg.com/vi/<?php echo $video_id; ?>/hqdefault.jpg" alt="">
+            </a>
+            <figcaption><?php echo $portfolio_title; ?></figcaption>
+          </figure>
+        </article>
+        <?php endforeach; ?>
+      </div>
+      <div class="see_all trans-all-2">
+        <a href="javascript:void(0);" title="<?php pll_e('See All'); ?>" class="btn btn-transparent btn-icon-right">
+          <span><?php pll_e('See All'); ?></span>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-down.svg" alt="" class="svg-icon">
+        </a>
       </div>
     </div>
+  </div>
   <?php endforeach; ?>
 </section>
 
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/scripts/magnific/magnific.css">
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/magnific/magnific.js"></script>
 <script>
-  $(".portfolio .portfolio_item .container .portfolio_items").each(function() {
-    var $this = $(this);
-    var $portfolio_items = $this.find("article").length;
-    if ($portfolio_items > 3) {
-      $this.next(".see_all").css({
-        "display": "flex"
-      })
-    }
-  });
-  $(".see_all").each(function() {
-    var $this = $(this);
-    $this.click(function() {
-      $this.prev(".portfolio_items").find("article").css({
-        "display": "block"
-      });
-      $this.hide();
-    });
-  });
-  if ($(".magnific_video").length) {
-    $(".magnific_video").magnificPopup({
-      disableOn: 700,
-      type: 'iframe',
-      mainClass: 'mfp-fade',
-      removalDelay: 160,
-      preloader: false,
-      fixedContentPos: false
-    });
+$(".portfolio .portfolio_item .container .portfolio_items").each(function() {
+  var $this = $(this);
+  var $portfolio_items = $this.find("article").length;
+  if ($portfolio_items > 3) {
+    $this.next(".see_all").css({
+      "display": "flex"
+    })
   }
+});
+$(".see_all").each(function() {
+  var $this = $(this);
+  $this.click(function() {
+    $this.prev(".portfolio_items").find("article").css({
+      "display": "block"
+    });
+    $this.hide();
+  });
+});
+if ($(".magnific_video").length) {
+  $(".magnific_video").magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+}
 </script>
 
 <?php
@@ -119,14 +119,14 @@ $testimonials_items = get_field("testimonials_items");
       <div class="testimonial_slider swiper">
         <div class="swiper-wrapper">
           <?php foreach ($testimonials_items as $item) : ?>
-            <?php
+          <?php
             $text = $item["text"];
             $author = $item["author"];
             ?>
-            <div class="swiper-slide">
-              <?php echo $text; ?>
-              <h4>- <?php echo $author; ?></h4>
-            </div>
+          <div class="swiper-slide">
+            <?php echo $text; ?>
+            <h4>- <?php echo $author; ?></h4>
+          </div>
           <?php endforeach; ?>
         </div>
         <div class="testimonial_slider_nav trans-all-2">
@@ -143,109 +143,82 @@ $testimonials_items = get_field("testimonials_items");
   </div>
 </section>
 
+<?php
+
+$cv_columns = get_field("cv_columns");
+
+?>
+
 <section class="cv">
   <img src="<?php echo get_template_directory_uri(); ?>/images/curve.svg" alt="" class="curves top">
   <div class="container">
+    <?php foreach ($cv_columns as $column) : ?>
+    <?php
+      $column_title = $column["column_title"];
+      $column_items = $column["column_items"];
+      ?>
     <div class="cv_column">
-      <h2>გამოცდილება</h2>
+      <h2><?php echo $column_title; ?></h2>
       <ul>
+        <?php foreach ($column_items as $item) : ?>
+        <?php
+            $image = $item["image"];
+            $picture = $image["sizes"]["cv-listing"];
+            $title = $item["title"];
+            $features = $item["features"];
+            $duration = $item["duration"];
+            ?>
         <li>
           <figure>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/cv-who.jpg" alt="">
+            <img src="<?php echo $picture; ?>" alt="">
           </figure>
           <div class="cv_texts">
-            <h3>Consultant</h3>
+            <h3><?php echo $title; ?></h3>
             <div class="text">
               <ul>
-                <li>World Health Organization</li>
+                <?php foreach ($features as $feature) : ?>
+                <?php
+                $title = $feature["title"];
+                ?>
+                <li><?php echo $title; ?></li>
+                <?php endforeach; ?>
               </ul>
-              <div class="period">Mar 2023 - Present</div>
+              <div class="duration"><?php echo $duration; ?></div>
             </div>
           </div>
         </li>
-        <li>
-          <figure>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/cv-health.jpg" alt="">
-          </figure>
-          <div class="cv_texts">
-            <h3>Founder / Nutritionist</h3>
-            <div class="text">
-              <ul>
-                <li>Health Design by RG</li>
-              </ul>
-              <div class="period">Oct 2022 - Present</div>
-            </div>
-          </div>
-        </li>
-        <li>
-          <figure>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/cv-erasmus.jpg" alt="">
-          </figure>
-          <div class="cv_texts">
-            <h3>Public Health Consultant</h3>
-            <div class="text">
-              <ul>
-                <li>Erasmus MC - Evaluation of Screening</li>
-              </ul>
-              <div class="period">May 2021 - Present</div>
-            </div>
-          </div>
-        </li>
+        <?php endforeach; ?>
       </ul>
+      <div class="see_all_cv trans-all-2">
+        <a href="javascript:void(0);" title="<?php pll_e('See All'); ?>" class="btn btn-transparent btn-icon-right">
+          <span><?php pll_e('See All'); ?></span>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-down.svg" alt="" class="svg-icon">
+        </a>
+      </div>
     </div>
-    <div class="cv_column">
-      <h2>განათლება</h2>
-      <ul>
-        <li>
-          <figure>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/cv-westminster.jpg" alt="">
-          </figure>
-          <div class="cv_texts">
-            <h3>University of Westminster</h3>
-            <div class="text">
-              <ul>
-                <li>Doctor of Philosophy - PhD</li>
-                <li>Public health nutrition</li>
-              </ul>
-              <div class="period">2021 - 2025</div>
-            </div>
-          </div>
-        </li>
-        <li>
-          <figure>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/cv-westminster.jpg" alt="">
-          </figure>
-          <div class="cv_texts">
-            <h3>University of Westminster</h3>
-            <div class="text">
-              <ul>
-                <li>BSc Human Nutrition</li>
-                <li>Human Nutrition</li>
-                <li>First Class Honors</li>
-              </ul>
-              <div class="period">2017 - 2020</div>
-            </div>
-          </div>
-        </li>
-        <li>
-          <figure>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/cv-tsmu.jpg" alt="">
-          </figure>
-          <div class="cv_texts">
-            <h3>Tbilisi State Medical University</h3>
-            <div class="text">
-              <ul>
-                <li>Bachelor's degree</li>
-                <li>BSc Public Health</li>
-                <li>A</li>
-              </ul>
-              <div class="period">2016 - 2017</div>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </div>
+    <?php endforeach; ?>
   </div>
 </section>
+
+<script>
+$(".cv .container .cv_column>ul").each(function() {
+  var $this = $(this);
+  var $portfolio_items = $this.find(">li").length;
+  if ($portfolio_items > 3) {
+    $this.next(".see_all_cv").css({
+      "display": "flex"
+    })
+  }
+});
+$(".see_all_cv").each(function() {
+  var $this = $(this);
+  $this.click(function() {
+    $this.prev("ul").find(">li").css({
+      "display": "grid"
+    });
+    $this.hide();
+  });
+});
+</script>
 
 <?php get_footer(); ?>
