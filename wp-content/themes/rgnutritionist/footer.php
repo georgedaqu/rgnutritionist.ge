04be5fff577be6e6d6477ab0413d1951e889faad
daqu <georgedaqu@gmail.com>
2024-01-05 18:24:24 +0400

    testimonials html

--- wp-content/themes/rgnutritionist/footer.php
+++ wp-content/themes/rgnutritionist/footer.php
@@ -8,33 +8,33 @@
       ?>
       <ul>
         <?php foreach ($contect_items as $item) : ?>
-        <li>
-          <?php if ($item['icon']) : ?>
-          <figure>
-            <img src="<?php echo $item['icon']; ?>" alt="">
-          </figure>
-          <?php endif; ?>
-          <div class="contact_texts">
-            <?php if ($item['subtitle']) : ?>
-            <h6>
-              <?php echo $item['subtitle']; ?>
-            </h6>
+          <li>
+            <?php if ($item['icon']) : ?>
+              <figure>
+                <img src="<?php echo $item['icon']; ?>" alt="">
+              </figure>
             <?php endif; ?>
-            <?php if ($item['item_type'] == "email") : ?>
-            <a href="mailto:<?php echo $item['email']; ?>" title="<?php echo $item['email']; ?>"><?php echo $item['email']; ?></a>
-            <?php endif; ?>
-            <?php if ($item['item_type'] == "phone") : ?>
-            <a href="tel:<?php echo $item['phone']; ?>" title="<?php echo $item['phone']; ?>"><?php echo $item['phone']; ?></a>
-            <?php endif; ?>
-            <?php if ($item['item_type'] == "link") : ?>
-            <?php $link = $item['link']; ?>
-            <a href="<?php echo $link['url']; ?>" title="<?php echo $link['title']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
-            <?php endif; ?>
-            <?php if ($item['item_type'] == "text") : ?>
-            <span><?php echo $item['text']; ?></span>
-            <?php endif; ?>
-          </div>
-        </li>
+            <div class="contact_texts">
+              <?php if ($item['subtitle']) : ?>
+                <h6>
+                  <?php echo $item['subtitle']; ?>
+                </h6>
+              <?php endif; ?>
+              <?php if ($item['item_type'] == "email") : ?>
+                <a href="mailto:<?php echo $item['email']; ?>" title="<?php echo $item['email']; ?>"><?php echo $item['email']; ?></a>
+              <?php endif; ?>
+              <?php if ($item['item_type'] == "phone") : ?>
+                <a href="tel:<?php echo $item['phone']; ?>" title="<?php echo $item['phone']; ?>"><?php echo $item['phone']; ?></a>
+              <?php endif; ?>
+              <?php if ($item['item_type'] == "link") : ?>
+                <?php $link = $item['link']; ?>
+                <a href="<?php echo $link['url']; ?>" title="<?php echo $link['title']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
+              <?php endif; ?>
+              <?php if ($item['item_type'] == "text") : ?>
+                <span><?php echo $item['text']; ?></span>
+              <?php endif; ?>
+            </div>
+          </li>
         <?php endforeach; ?>
       </ul>
     </address>
@@ -42,44 +42,57 @@
 </footer>
 
 <script>
-var partners_slider = new Swiper(".partners_slider", {
-  speed: 1000,
-  loop: true,
-  slidesPerView: 2,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".partners_slider_next",
-    prevEl: ".partners_slider_prev",
-  },
-  breakpoints: {
-    1280: {
-      slidesPerView: 5,
-      spaceBetween: 30
+  var partners_slider = new Swiper(".partners_slider", {
+    speed: 1000,
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".partners_slider_next",
+      prevEl: ".partners_slider_prev",
     },
-    768: {
-      slidesPerView: 5,
-      spaceBetween: 20
-    },
-    576: {
-      slidesPerView: 4,
-      spaceBetween: 20
+    breakpoints: {
+      1280: {
+        slidesPerView: 5,
+        spaceBetween: 30
+      },
+      768: {
+        slidesPerView: 5,
+        spaceBetween: 20
+      },
+      576: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      }
     }
-  }
-});
-$('img.svg-icon').each(function() {
-  var $img = $(this);
-  var imgURL = $img.attr('src');
-  $.get(
-    imgURL,
-    function(data) {
-      var $svg = $(data).find('svg');
-      $svg = $svg.attr('class', $img.attr('class'));
-      $svg = $svg.removeAttr('xmlns:a');
-      $img.replaceWith($svg);
+  });
+  var testimonial_slider = new Swiper(".testimonial_slider", {
+    speed: 1000,
+    loop: true,
+    slidesPerView: 1,
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true
     },
-    'xml'
-  );
-});
+    navigation: {
+      nextEl: ".testimonial_slider_next",
+      prevEl: ".testimonial_slider_prev",
+    }
+  });
+  $('img.svg-icon').each(function() {
+    var $img = $(this);
+    var imgURL = $img.attr('src');
+    $.get(
+      imgURL,
+      function(data) {
+        var $svg = $(data).find('svg');
+        $svg = $svg.attr('class', $img.attr('class'));
+        $svg = $svg.removeAttr('xmlns:a');
+        $img.replaceWith($svg);
+      },
+      'xml'
+    );
+  });
 </script>
 
 <?php wp_footer(); ?>
--- wp-content/themes/rgnutritionist/templates/about-us.php
+++ wp-content/themes/rgnutritionist/templates/about-us.php
@@ -71,6 +71,40 @@ get_header();
   <?php endforeach; ?>
 </section>
 
+<section class="testimoinals">
+  <div class="container">
+    <div class="testimonial_body">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/testimonial.svg" alt="" class="border">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/quot.svg" alt="" class="quot">
+      <div class="testimonial_slider swiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <p>კვება არ არის მხოლოდ ენერგია, არამედ ჯანმრთელობის, კულტურის, კომუნიკაციისა და ყოველდღიურობის დიდი ნაწილი</p>
+            <h4>- ქეთი</h4>
+          </div>
+          <div class="swiper-slide">
+            <p>დაბალანსებული კვება დაგეხმარება გახდეთ მეტად პროდუქტიული, გაიუმჯობესოთ ჯანმრთელობა და ცხოვრების ხარისხი!</p>
+            <h4>- სალომე</h4>
+          </div>
+          <div class="swiper-slide">
+            <p>კვების ბალანსი ჯანმრთელობის ერთ-ერთი საუკეთესო ინვესტიციაა თქვენი მომავლისთვის!</p>
+            <h4>- ლუკა</h4>
+          </div>
+        </div>
+        <div class="testimonial_slider_nav trans-all-2">
+          <a href="javascript:void(0);" class="testimonial_slider_prev">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-left.svg" alt="" class="svg-icon">
+          </a>
+          <a href="javascript:void(0);" class="testimonial_slider_next">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-right.svg" alt="" class="svg-icon">
+          </a>
+        </div>
+      </div>
+    </div>
+    <h2>კმაყოფილი მომხმარებელი</h2>
+  </div>
+</section>
+
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/scripts/magnific/magnific.css">
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/magnific/magnific.js"></script>
 <script>
