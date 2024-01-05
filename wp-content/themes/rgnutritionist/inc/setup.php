c2361660ece5230bda49b9f101eabbe43e075b94
daqu <georgedaqu@gmail.com>
2024-01-06 00:32:48 +0400

    cv process

--- wp-content/themes/rgnutritionist/inc/setup.php
+++ wp-content/themes/rgnutritionist/inc/setup.php
@@ -23,6 +23,7 @@ remove_image_size('2048x2048');
 add_image_size('blog-featured', 920, 550, true);
 add_image_size('services-listing', 240, 240, true);
 add_image_size('portfolio-listing', 330, 185, true);
+add_image_size('cv-listing', 90, 90, true);
 // Cropped JPG quality 100%
 add_filter('jpeg_quality', function ($arg) {
 	return 100;
--- wp-content/themes/rgnutritionist/templates/about-us.php
+++ wp-content/themes/rgnutritionist/templates/about-us.php
@@ -31,77 +31,77 @@ get_header();
   $portfolio_row = get_field("portfolio_row");
   ?>
   <?php foreach ($portfolio_row as $portfolio_item) : ?>
-  <?php
+    <?php
     $portfolio_row_title = $portfolio_item["portfolio_row_title"];
     $portfolio_item = $portfolio_item["portfolio_item"];
     ?>
-  <div class="portfolio_item">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/worm-right.svg" alt="" class="worm">
-    <div class="container">
-      <h2><?php echo $portfolio_row_title; ?></h2>
-      <div class="portfolio_items">
-        <?php foreach ($portfolio_item as $item) : ?>
-        <?php
+    <div class="portfolio_item">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/worm-right.svg" alt="" class="worm">
+      <div class="container">
+        <h2><?php echo $portfolio_row_title; ?></h2>
+        <div class="portfolio_items">
+          <?php foreach ($portfolio_item as $item) : ?>
+            <?php
             $portfolio_video = $item["portfolio_video"];
             $portfolio_title = $item["portfolio_title"];
             ?>
-        <article>
-          <figure>
-            <a href="<?php echo $portfolio_video; ?>" class="magnific_video">
-              <?php
+            <article>
+              <figure>
+                <a href="<?php echo $portfolio_video; ?>" class="magnific_video">
+                  <?php
                   $url = $portfolio_video;
                   parse_str(parse_url($url, PHP_URL_QUERY), $query_params);
                   $video_id = $query_params['v'];
                   ?>
-              <img src="https://i.ytimg.com/vi/<?php echo $video_id; ?>/hqdefault.jpg" alt="">
-            </a>
-            <figcaption><?php echo $portfolio_title; ?></figcaption>
-          </figure>
-        </article>
-        <?php endforeach; ?>
-      </div>
-      <div class="see_all trans-all-2">
-        <a href="javascript:void(0);" title="ნახე ყველა" class="btn btn-transparent btn-icon-right">
-          <span>ნახე ყველა</span>
-          <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-down.svg" alt="" class="svg-icon">
-        </a>
+                  <img src="https://i.ytimg.com/vi/<?php echo $video_id; ?>/hqdefault.jpg" alt="">
+                </a>
+                <figcaption><?php echo $portfolio_title; ?></figcaption>
+              </figure>
+            </article>
+          <?php endforeach; ?>
+        </div>
+        <div class="see_all trans-all-2">
+          <a href="javascript:void(0);" title="ნახე ყველა" class="btn btn-transparent btn-icon-right">
+            <span>ნახე ყველა</span>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-down.svg" alt="" class="svg-icon">
+          </a>
+        </div>
       </div>
     </div>
-  </div>
   <?php endforeach; ?>
 </section>
 
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/scripts/magnific/magnific.css">
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/magnific/magnific.js"></script>
 <script>
-$(".portfolio .portfolio_item .container .portfolio_items").each(function() {
-  var $this = $(this);
-  var $portfolio_items = $this.find("article").length;
-  if ($portfolio_items > 3) {
-    $this.next(".see_all").css({
-      "display": "flex"
-    })
-  }
-});
-$(".see_all").each(function() {
-  var $this = $(this);
-  $this.click(function() {
-    $this.prev(".portfolio_items").find("article").css({
-      "display": "block"
+  $(".portfolio .portfolio_item .container .portfolio_items").each(function() {
+    var $this = $(this);
+    var $portfolio_items = $this.find("article").length;
+    if ($portfolio_items > 3) {
+      $this.next(".see_all").css({
+        "display": "flex"
+      })
+    }
+  });
+  $(".see_all").each(function() {
+    var $this = $(this);
+    $this.click(function() {
+      $this.prev(".portfolio_items").find("article").css({
+        "display": "block"
+      });
+      $this.hide();
     });
-    $this.hide();
   });
-});
-if ($(".magnific_video").length) {
-  $(".magnific_video").magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false
-  });
-}
+  if ($(".magnific_video").length) {
+    $(".magnific_video").magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: false
+    });
+  }
 </script>
 
 <?php
@@ -119,14 +119,14 @@ $testimonials_items = get_field("testimonials_items");
       <div class="testimonial_slider swiper">
         <div class="swiper-wrapper">
           <?php foreach ($testimonials_items as $item) : ?>
-          <?php
+            <?php
             $text = $item["text"];
             $author = $item["author"];
             ?>
-          <div class="swiper-slide">
-            <?php echo $text; ?>
-            <h4>- <?php echo $author; ?></h4>
-          </div>
+            <div class="swiper-slide">
+              <?php echo $text; ?>
+              <h4>- <?php echo $author; ?></h4>
+            </div>
           <?php endforeach; ?>
         </div>
         <div class="testimonial_slider_nav trans-all-2">
@@ -140,6 +140,111 @@ $testimonials_items = get_field("testimonials_items");
       </div>
     </div>
     <h2><?php echo $testimonials_title; ?></h2>
+  </div>
+</section>
+
+<section class="cv">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/curve.svg" alt="" class="curves top">
+  <div class="container">
+    <div class="cv_column">
+      <h2>გამოცდილება</h2>
+      <ul>
+        <li>
+          <figure>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/cv-who.jpg" alt="">
+          </figure>
+          <div class="cv_texts">
+            <h3>Consultant</h3>
+            <div class="text">
+              <ul>
+                <li>World Health Organization</li>
+              </ul>
+              <div class="period">Mar 2023 - Present</div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <figure>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/cv-health.jpg" alt="">
+          </figure>
+          <div class="cv_texts">
+            <h3>Founder / Nutritionist</h3>
+            <div class="text">
+              <ul>
+                <li>Health Design by RG</li>
+              </ul>
+              <div class="period">Oct 2022 - Present</div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <figure>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/cv-erasmus.jpg" alt="">
+          </figure>
+          <div class="cv_texts">
+            <h3>Public Health Consultant</h3>
+            <div class="text">
+              <ul>
+                <li>Erasmus MC - Evaluation of Screening</li>
+              </ul>
+              <div class="period">May 2021 - Present</div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="cv_column">
+      <h2>განათლება</h2>
+      <ul>
+        <li>
+          <figure>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/cv-westminster.jpg" alt="">
+          </figure>
+          <div class="cv_texts">
+            <h3>University of Westminster</h3>
+            <div class="text">
+              <ul>
+                <li>Doctor of Philosophy - PhD</li>
+                <li>Public health nutrition</li>
+              </ul>
+              <div class="period">2021 - 2025</div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <figure>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/cv-westminster.jpg" alt="">
+          </figure>
+          <div class="cv_texts">
+            <h3>University of Westminster</h3>
+            <div class="text">
+              <ul>
+                <li>BSc Human Nutrition</li>
+                <li>Human Nutrition</li>
+                <li>First Class Honors</li>
+              </ul>
+              <div class="period">2017 - 2020</div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <figure>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/cv-tsmu.jpg" alt="">
+          </figure>
+          <div class="cv_texts">
+            <h3>Tbilisi State Medical University</h3>
+            <div class="text">
+              <ul>
+                <li>Bachelor's degree</li>
+                <li>BSc Public Health</li>
+                <li>A</li>
+              </ul>
+              <div class="period">2016 - 2017</div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </section>
 
