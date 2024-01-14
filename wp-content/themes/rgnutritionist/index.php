965b46681c45409c9c8eedb0ffe01cd4f7daf3cb
daqu <georgedaqu@gmail.com>
2024-01-14 18:02:02 +0400

    blog

--- wp-content/themes/rgnutritionist/index.php
+++ wp-content/themes/rgnutritionist/index.php
@@ -1,5 +1,38 @@
 <?php get_header(); ?>
 
+<section class="page_title">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/curve-gray-top.svg" alt="" class="curves top">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/curve-gray-bot.svg" alt="" class="curves bot">
+  <div class="container">
+    <h1><?php pll_e("Blog"); ?></h1>
+  </div>
+</section>
 
+<section class="blog_listing trans-all-4">
+  <div class="container">
+    <?php
+    $args = array(
+      'post_type' => 'post',
+      'posts_per_page' => 10,
+    );
+    ?>
+    <?php $query = new WP_Query($args); ?>
+    <?php if ($query->have_posts()) : ?>
+      <?php while ($query->have_posts()) : ?>
+        <?php $query->the_post(); ?>
+        <article>
+          <figure>
+            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+              <?php the_post_thumbnail('blog-featured'); ?>
+            </a>
+          </figure>
+          <time><?php echo get_the_date(); ?></time>
+          <h3><?php the_title(); ?></h3>
+        </article>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?>
+    <?php endif; ?>
+  </div>
+</section>
 
 <?php get_footer(); ?>
--- wp-content/themes/rgnutritionist/single.php
+++ wp-content/themes/rgnutritionist/single.php
@@ -1,5 +1,24 @@
 <?php get_header(); ?>
 
+<section class="page_title">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/curve-gray-top.svg" alt="" class="curves top">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/curve-gray-bot.svg" alt="" class="curves bot">
+  <div class="container">
+    <h1><?php pll_e("Blog"); ?></h1>
+  </div>
+</section>
 
+<section class="article_page trans-all-2">
+  <div class="container narrow">
+    <figure class="featured_image">
+      <?php the_post_thumbnail('blog-featured'); ?>
+    </figure>
+    <time><?php echo get_the_date(); ?></time>
+    <h2><?php the_title(); ?></h2>
+    <div class="blog_content">
+      <?php the_content(); ?>
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
